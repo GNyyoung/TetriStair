22f5aa7f8eaf0470617109b8fcd6f92b0fb1fecd
GNyyoung <38244724+GNyyoung@users.noreply.github.com>
2019-01-22 22:13:03 +0900

    Initial commit

---
+++ Assets/Scripts/BlockArrayManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockArrayManager : MonoBehaviour {
+
+    public enum Arr { Empty, Block, Character}
+
+    private int[,] gameArray = new int[10, 17]; //맨 위 3칸은 안보이게 한다.
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //한 행이 블럭으로 찼는지 확인한다.
+    //true가 나올 경우 보너스 발동하는 메서드에 연결
+    public bool CheckRow(int[] rowIndex)
+    {
+        bool isComplete = true;
+        for(int i = 0; i < rowIndex.Length; i++)
+        {
+            for(int k = 0; k < gameArray.GetLength(1); k++)
+            {
+                if (gameArray[i, k] != 1)
+                    isComplete = false;
+            }
+            if (isComplete == true)
+                return true;
+        }
+        return false;
+    }
+
+    public int[,] GetGameArray()
+    {
+        return gameArray;
+    }
+
+    public int GetCollisionDistance(int indexX, int indexY)
+    {
+        int distance = 0;
+
+        while(indexY < gameArray.GetLength(1))
+        {
+            if (gameArray[indexX, indexY] == 0)
+            {
+                distance += 1;
+            }
+            else if (gameArray[indexX, indexY] == 2)
+            {
+                //게임오버 메서드 실행
+            }
+            indexY += 1;
+        }
+
+        return distance;
+    }
+}
---
+++ Assets/Scripts/BlockController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockController : MonoBehaviour {
+
+    public Module[] controlBlock = new Module[4];
+
+    public struct Module
+    {
+        public int indexX, indexY;
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //조종중인 블럭을 회전시킨다.
+    public void RotateBlock(bool direction)
+    {
+
+    }
+
+    //블럭이 시간에 따라 자동으로 내려갈 때 사용하는 메서드
+    public void FallBlock()
+    {
+
+    }
+
+    //블럭을 빠른 낙하시킬 때 사용하는 메서드
+    public void FastFallBlock()
+    {
+        int distance = int.MaxValue;
+        for(int i = 0; i < controlBlock.Length; i++)
+        {
+            //controlBlock[i].indexY
+            GetComponent<BlockArrayManager>().GetCollisionDistance();
+        }
+    }
+}
